7d0ed1d1871b5986713e49b0cbbd4ec8ab4a1440
kch <kch@inmedias.it>
2022-09-26 10:03:19 +0200

    Fix for-loop in get_weather_data

--- api/utils.py
+++ api/utils.py
@@ -11,6 +11,109 @@ retry_strategy = Retry(
 adapter = HTTPAdapter(max_retries=retry_strategy)
 geolocator = Nominatim(user_agent="geotest")
 
+# WEATHER_DESCRIPTION = {
+#     0: "Clear sky",
+#     1: "Mainly clear",
+#     2: "partly cloudy",
+#     3: "overcast",
+#     45: "Fog",
+#     48: "depositing rime fog",
+#     51: "Drizzle: Light",
+#     53: "Drizzle: moderate",
+#     55: "Drizzle: dense intensity",
+#     56: "Freezing Drizzle: Light",
+#     57: "Freezing Drizzle: dense intensity",
+#     61: "Rain: Slight",
+#     63: "Rain: moderate",
+#     65: "Rain: heavy intensity",
+#     66: "Freezing Rain: Light",
+#     67: "Freezing Rain: heavy intensity",
+#     71: "Snow fall: Slight",
+#     73: "Snow fall: moderate",
+#     75: "Snow fall: heavy intensity",
+#     77: "Snow grains",
+#     80: "Rain showers: Slight",
+#     81: "Rain showers: moderate",
+#     82: "Rain showers: violent",
+#     85: "Snow showers slight",
+#     86: "Snow showers heavy",
+#     95: "Thunderstorm: Slight or moderate",
+#     96: "Thunderstorm with slight",
+#     99: "Thunderstorm with heavy hail"
+# }
+
+WEATHER_DESCRIPTION = {
+    0: "Sonne",
+    1: "Sonne",
+    2: "Sonne hinter kleiner Wolke",
+    3: "Sonne hinter großer Wolke",
+    45: "Nebel",
+    48: "Nebel",
+    51: "Sonne mit heller Regenwolke und wenig Regen",
+    53: "Sonne mit heller Regenwolke und wenig Regen",
+    55: "Sonne mit dunkler Regenwolke und wenig Regen",
+    56: "Sonne mit heller Regenwolke und wenig Regen",
+    57: "Sonne mit dunkler Regenwolke und wenig Regen",
+    61: "Sonne mit heller Regenwolke und wenig Regen",
+    63: "Sonne mit heller Regenwolke und wenig Regen",
+    65: "Sonne mit dunkler Regenwolke und wenig Regen",
+    66: "Sonne mit heller Regenwolke und wenig Regen",
+    67: "Sonne mit dunkler Regenwolke und wenig Regen",
+    71: "Regenwolke",
+    73: "Regenwolke",
+    75: "Regenwolke",
+    77: "Regenwolke",
+    80: "Sonne mit heller Regenwolke und wenig Regen",
+    81: "Sonne mit heller Regenwolke und wenig Regen",
+    82: "Sonne mit dunkler Regenwolke und wenig Regen",
+    85: "Regenwolke",
+    86: "Regenwolke",
+    95: "Mond",
+    96: "Mond",
+    99: "Mond"
+}
+
+WEATHER_ICONS = {
+    "Sonne hinter großer Wolke": {"icon": "mdi-weather-cloudy", "color": "grey", "description": "Bewölkt"},
+    "Helle Wolke": {"icon": "mdi-weather-cloudy", "color": "lightgrey", "description": "Bewölkt"},
+
+    "Mond hinter großer Wolke": {"icon": "mdi-weather-cloudy", "color": "grey", "description": "Bewölkt"},
+
+    "Kleine Sonne mit dunkler Wolke": {"icon": "mdi-weather-cloudy", "color": "grey", "description": "Bewölkt"},
+    "Sonne hinter kleiner Wolke": {"icon": "mdi-weather-partly-cloudy", "color": "lightgrey",
+                                   "description": "Leicht Bewölkt"},
+
+    "Dunkle Wolke mit wenig Regen": {"icon": "mdi-weather-partly-rainy", "color": "darkblue",
+                                     "description": "Leichter Regen"},
+    "Sonne mit dunkler Regenwolke und wenig Regen": {"icon": "mdi-weather-partly-rainy", "color": "grey",
+                                                     "description": "Leichter Regen"},
+    "Sonne mit heller Regenwolke und wenig Regen": {"icon": "mdi-weather-partly-rainy", "color": "lightgrey",
+                                                    "description": "Leichter Regen"},
+    "Mond mit dunkler Regenwolke und wenig Regen": {"icon": "mdi-weather-partly-rainy", "color": "grey",
+                                                    "description": "Leichter Regen"},
+    "Mond mit heller Regenwolke und wenig Regen": {"icon": "mdi-weather-partly-rainy", "color": "lightgrey",
+                                                   "description": "Leichter Regen"},
+    "Sonne mit Regenwolke": {"icon": "mdi-weather-partly-rainy", "color": "lightblue", "description": "Regen"},
+    "Regenwolke": {"icon": "mdi-weather-rainy", "color": "blue", "description": "Regen"},
+
+    "Mond hinter kleiner Wolke": {"icon": "mdi-eather-night-partly-cloudy", "color": "lightgrey",
+                                  "description": "Leicht Bewölkt"},
+    "Kleiner Mond mit dunkler Wolke": {"icon": "mdi-weather-night-partly-cloudy", "color": "grey",
+                                       "description": "Bewölkt"},
+    "Mond": {"icon": "mdi-weather-night", "color": "grey", "description": "Klar"},
+
+    "Sonne": {"icon": "mdi-weather-sunny", "color": "yellow", "description": "Klar"},
+    "Sonnenaufgang": {"icon": "mdi-weather-sunny", "color": "yellow", "description": "Klar"},
+    "Sonnenuntergang": {"icon": "mdi-weather-sunny", "color": "yellow", "description": "Klar"},
+
+    "Nebel": {"icon": "mdi-weather-fog", "color": "grey", "description": "Nebel"}
+}
+
+
+def get_icon_by_weathercode(weathercode):
+    weather_description = WEATHER_DESCRIPTION[weathercode]
+    print(WEATHER_ICONS[weather_description])
+
 
 def get_weather_data(location):
     location = geolocator.geocode(location)
@@ -36,38 +139,31 @@ def get_weather_data(location):
     current_data = data['current_weather']
     hourly_data = data['hourly']
     daily_data = data['daily']
-    weather_time_list = []
 
     for i in range(7):
-        for j in range(168):
-            weather_times = {
-                hourly_data['time'][j][11:13]: {
-                    'temp': hourly_data['temperature_2m'][j],
-                    'cloudcover': hourly_data['cloudcover'][j],
-                    'precipitation': hourly_data['precipitation'][j],
-                    'snowfall': hourly_data['snowfall'][j],
-                    'weathercode': hourly_data['weathercode'][j],
+        weather_times = {}
+        for j in range(24):
+            weather_times[hourly_data['time'][j][11:13]] = {
+                    'temp': hourly_data['temperature_2m'][j + i * 24],
+                    'cloudcover': hourly_data['cloudcover'][j + i * 24],
+                    'precipitation': hourly_data['precipitation'][j + i * 24],
+                    'snowfall': hourly_data['snowfall'][j + i * 24],
+                    'weathercode': hourly_data['weathercode'][j + i * 24],
 
                 }
-            }
-            weather_time_list.append(weather_times)
 
-    # chunk weather_time_list to put into daily data
-        our_list = weather_time_list
-        chunk_size = 24
-        chunked_list = [our_list[i:i + chunk_size] for i in range(0, len(our_list), chunk_size)]
 
         daily_weather_data = {
             daily_data['time'][i]: {
-                    'temp_max': daily_data['temperature_2m_max'][i],
-                    'temp_min': daily_data['temperature_2m_min'][i],
-                    'precipitation_sum': daily_data['precipitation_sum'][i],
-                    'snowfall_sum': daily_data['snowfall_sum'][i],
-                    'temp_current': current_data['temperature'],
-                    'weather_times': chunked_list[i]
+                'temp_max': daily_data['temperature_2m_max'][i],
+                'temp_min': daily_data['temperature_2m_min'][i],
+                'precipitation_sum': daily_data['precipitation_sum'][i],
+                'snowfall_sum': daily_data['snowfall_sum'][i],
+                'temp_current': current_data['temperature'],
+                'weather_times': weather_times
             }
         }
         print(daily_weather_data)
 
 
-get_weather_data('seoul')
+get_weather_data('incheon')
