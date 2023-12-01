c13a7bd19ed9c901c89f3abd478327496a54573e
Lawrence <lta@inmedias.it>
2023-12-01 09:35:30 +0100

    refaktorierte location_detail für deutliche 'error_response_handling'

--- api/apps/weather/views.py
+++ api/apps/weather/views.py
@@ -31,38 +31,7 @@ def location_list(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# ***Original Version: location_detail***
-# @api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-# def location_detail(request, pk):
-#     """
-#     API view zum Abrufen, Aktualisieren oder Löschen eines Orts.
-#     """
-#     try:
-#         location = Location.objects.get(pk=pk)
-#     except ObjectDoesNotExist:
-#         return Response(status=status.HTTP_404_NOT_FOUND)
-#
-#     if request.method == "GET":
-#         serializer = LocationSerializer(location)
-#         return Response(serializer.data)
-#
-#     elif request.method == "PUT":
-#         serializer = LocationSerializer(location, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     elif request.method == "PATCH":
-#         serializer = LocationSerializer(location, data=request.data, partial=True)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_206_PARTIAL_CONTENT)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#
-#     elif request.method == "DELETE":
-#         location.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # ***Modified location_detail: prepped for unit tests***
 
